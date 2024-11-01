ac04dc1011ee96be65239a1601e787dab1fe7b54
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-11-01 21:05:12 +0530

    second commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import "./Home.css";
 import Header from '../UI/Header/Header';
+import ExploreMenu from '../ExploreMenu/ExploreMenu';
 
 
 const Home = () => {
   return (
     <div>
       <Header />
+      <ExploreMenu />
     </div>
   )
 }
