5841a0a9dc31436abcdb1cbd02a442717e255472
Nikhat Parvin <nikhat.parvin013@gmail.com>
2024-11-01 23:31:29 +0530

    Third Commit

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Home.css";
 import Header from '../UI/Header/Header';
 import ExploreMenu from '../ExploreMenu/ExploreMenu';
+import FoodDisplay from '../foodDisplay/FoodDisplay';
 
 
 const Home = () => {
+  const [category,setCategory] = useState("All");
   return (
     <div>
       <Header />
-      <ExploreMenu />
+      <ExploreMenu category={category} setCategory={setCategory} />
+      <FoodDisplay category={category} />
     </div>
   )
 }
