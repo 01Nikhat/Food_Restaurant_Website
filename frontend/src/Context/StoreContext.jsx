29650a1015b188348cfa63d1d0032c68a02b79f3
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-02-13 00:06:51 +0530

    first commit

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -1,3 +1,100 @@
+// import axios from "axios";
+// import { createContext, useEffect, useState } from "react";
+
+
+// export const StoreContext = createContext(null);
+
+// const StoreContextProvider = (props) => {
+
+//   const [cartItems,setCartItems] = useState({});
+
+//   const url = import.meta.env.VITE_BACKEND_URL;
+
+
+//  // const url =  process.env.backend_url;  //backend integration part
+//   const [token,setToken] = useState("")   //backend integration part
+//   //store foodlist item by admin will add
+//   const [food_list,setFoodlist] = useState([]);
+
+// //itemId main id value pass kar rahe hai 
+//   const addToCart = async(itemId) => {
+//     //check carItems[itemId] main 0 value present  hai the if statement true or cardItem mai 1 assign ho jayega 
+//     if(!cartItems[itemId]){
+//       setCartItems((prevVal)=>({...prevVal,[itemId]:1}))
+//       //preVal mai jo hai usko aise hi rahne do or or jis id; mai call hua hai uska cardItem 1 hoga like it will show id is 5 then it will show [5:1]
+//     }
+//     else{
+//       setCartItems((prevVal)=>({...prevVal,[itemId]:prevVal[itemId]+1}))
+//       //preVal mai jo hai usko aise hi rahne do or  jis id mai call hua hai uska cardItem +1 hoga like it will show id is 5 and preVal is is 2 ,then it will show [5:3] +1 add ho jayega 
+//     }
+//     if (token) {
+//       await axios.post(url+"/api/cart/add",{itemId},{headers:{token}})
+//     }
+//   }
+
+//   const removeFromCart = async(itemId) =>{
+//     setCartItems((prevVal)=>({...prevVal,[itemId]:prevVal[itemId]-1}));
+//     //preVal mai jo hai usko aise hi rahne do or  jis id mai call hua hai uska cardItem -1 hoga like it will show id is 5 and preVal is is 2 ,then it will show [5:1] -1 subtract ho jayega 
+//     if (token) {
+//       await axios.post(url+"/api/cart/remove",{itemId},{headers:{token}});
+//     }
+//   }
+//   const getTotalCartAmount = () =>{
+//     let totalAmaount = 0;
+//     for(const item in cartItems){
+//       if (cartItems[item]>0) {
+//         let itemInfo =  food_list.find((product)=>product._id === item);
+//         totalAmaount += itemInfo.price * cartItems[item];
+//       }
+      
+//     }
+//     return totalAmaount;
+//   }
+//   //backend integration part
+//   const fethFoodList = async () =>{
+//     const response = await axios.get(url+"/api/food/list");
+//     setFoodlist(response.data.data)
+//   }
+//   const loadCartData = async(token) =>{
+//       const response = await axios.post(url+"/api/cart/get",{},{headers:{token}});
+//       setCartItems(response.data.cartData);
+
+//   }
+//   useEffect(()=>{
+    
+//     async function loadData() {
+//       await fethFoodList();
+//       if (localStorage.getItem("token")) {
+//         setToken(localStorage.getItem("token"));
+//         await loadCartData(localStorage.getItem("token"));
+//     }
+//     }
+//     loadData();
+//   },[])
+//   const contextValue = {
+//     food_list,
+//     cartItems,
+//     setCartItems,
+//     addToCart,
+//     removeFromCart,
+//     getTotalCartAmount,
+//     url,   //backend integration part
+//     token,   //backend integration part
+//     setToken   //backend integration part
+//   };
+// // 
+
+
+//   return (
+//     <StoreContext.Provider value={contextValue}>
+//       {props.children}
+//     </StoreContext.Provider>
+//   );
+// };
+
+// export default StoreContextProvider;
+
+
 import axios from "axios";
 import { createContext, useEffect, useState } from "react";
 
