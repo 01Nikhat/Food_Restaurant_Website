39e40d20cfea5c63bdc1c27957d4af579a3f6d36
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-02-13 13:24:36 +0530

    Fixing missing files issue

--- Backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () =>{
-  await mongoose.connect('mongodb+srv://nikhatparvin:7687829892@cluster0.n7hc6.mongodb.net/food_website').then(()=>console.log("DB Connected"));
-}
