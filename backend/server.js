bdedb4b380e7f19b9c4b25ed3fb6a46ff6523548
Nikhat Parvin <nikhat.parvin013@gmail.com>
2025-02-15 19:06:31 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ export const instance = new Razorpay({
 
 // App config
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 // Middleware
 app.use(express.json());
